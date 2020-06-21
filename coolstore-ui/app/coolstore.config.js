0e938d73e42a3b8ca56dc0615fb8edc90acb8e8f
woyaowoyao <laidongshi@gmail.com>
2020-06-21 15:02:46 +0800

    Update coolstore.config.js

--- coolstore-ui/app/coolstore.config.js
+++ coolstore-ui/app/coolstore.config.js
@@ -2,7 +2,7 @@ var config =
 {
   API_ENDPOINT: (process.env.COOLSTORE_GW_ENDPOINT != null ? process.env.COOLSTORE_GW_ENDPOINT  : process.env.COOLSTORE_GW_SERVICE + '-' + process.env.OPENSHIFT_BUILD_NAMESPACE),
   SECURE_API_ENDPOINT: (process.env.SECURE_COOLSTORE_GW_ENDPOINT != null ? process.env.SECURE_COOLSTORE_GW_ENDPOINT  : process.env.SECURE_COOLSTORE_GW_SERVICE + '-' + process.env.SECURE_COOLSTORE_GW_SERVICE),
-  SSO_ENABLED: process.env.SSO_URL ? true : false
+  SSO_ENABLED: process.env.SSO_URL ? false : false
 };
 
 module.exports = config;
